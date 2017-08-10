527c77c1ea3fe21f8e4c7cb7050809c86df06e5b
Bhargava Ch <chbhargava@gmail.com>
2017-08-10 23:32:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,28 +15,12 @@ const wss = new SocketServer({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+  
   ws.on('close', () => console.log('Client disconnected'));
-});
-
-
-wss.on('request', function(request) {
-  console.log('On request!');
-  console.log(request);
-  var connection = request.accept(null, request.origin);
-
-  // This is the most important callback for us, we'll handle
-  // all messages from users here.
-  connection.on('message', function(message) {
-    console.log('Message received!');
-    console.log(message);
-    if (message.type === 'utf8') {
-      console.log('utf8');
-    }
-  });
-
-  connection.on('close', function(connection) {
-    console.log('Client closed');
-  });
 });
 
 /*
